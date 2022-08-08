96f54c7f06b0a46cb52a61a5b18fecb3d7124489
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-08-07 17:08:13 -0700

    updating ./terraform/tf-module-rabbitmq

--- rabbitmq.tf
+++ rabbitmq.tf
@@ -5,7 +5,7 @@ resource "aws_spot_instance_request" "instance" {
     wait_for_fulfillment   = true
     subnet_id = var.PRIVATE_SUBNET_IDS[0]
     vpc_security_group_ids = [aws_security_group.main.id]
-
+    iam_instance_profile = aws_iam_instance_profile.allow-secret-manager-read-access.name
   tags = {
     Name = local.TAG_PREFIX
   }
@@ -16,4 +16,17 @@ resource "aws_ec2_tag" "name-tag" {
   resource_id = aws_spot_instance_request.instance.spot_instance_id
   key         = "Name"
   value       = local.TAG_PREFIX
+}
+
+resource "null_resource" "ansible" {
+  provisioner "remote-exec" {
+    connection {
+      user     = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_USER"]
+      password = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_PASS"]
+      host     = aws_spot_instance_request.instance.private_ip
+    }
+    inline = [
+      "ansible-pull -U https://github.com/krishnavamsi7616/roboshop-ansible.git -e HOST=localhost -e ROLE=rabbitmq -e ENV=${var.ENV}",
+    ]
+  }
 }
